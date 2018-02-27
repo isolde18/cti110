41bb6a61e11a378782bbd305f6b8f6f988aa612f
silvia16 <36601499+silvia16@users.noreply.github.com>
2018-02-27 09:53:02 -0500

    Add files via upload

---
+++ score_stu.py
@@ -0,0 +1,26 @@
+#CTI110
+#Silvia
+#27.02.18
+#Calculate student's scores with if else
+
+#Define variables or/and get user input
+
+score = int (input ("Enter score value: "))
+
+#Calculatons or desicion structures
+
+
+if score >=90:
+    print ("A")
+else:
+    if score >= 80:
+        print ("B")
+    else:
+        if score >=70:
+            print ("C")
+        else:
+            if score>=60:
+               print ("D")
+            else:
+                  print ("F")
+
