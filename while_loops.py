96ebc90f8c156ea7b3501ed84c8e31e1f50edca5
silvia16 <36601499+silvia16@users.noreply.github.com>
2018-03-08 10:45:50 -0500

    Add files via upload

---
+++ while_loops.py
@@ -0,0 +1,12 @@
+
+
+keep_going='y' #set value of kee_going to y so loop iterates at least once
+while keep_going=='y':
+    sales=float(input("Enter sales amount: "))
+    comm_rate=float(input("Enter commison rate: "))            
+    commission= sales * comm_rate
+    print ("Your commission is ", commission )#display commision calculated
+    print () #to skip a line
+    #below statement provides means to terminate loop or continue
+    keep_going= input("Do you want to calculate another commission? Enter y if yes ")
+    print()
