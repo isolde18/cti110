5c8196ae4b60582838ea0f080190402b63562e45
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-03-12 16:29:34 -0400

    Add files via upload

---
+++ age_groups.py
@@ -0,0 +1,16 @@
+
+
+
+#What age are you?
+
+num= float(input("Enter num "))
+if num >=1 and num <13:
+    print ("The person is a child")
+    
+elif num >=13 and num < 20:
+    print ("The person is a teenager")
+
+elif num >=20:
+    print("The person is adult")
+else:
+    print ("The person is an infant")
