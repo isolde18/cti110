96ea9ba68d69295dc70a8c3fc6cc55279a58dbc8
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-03-22 10:45:11 -0400

    Add files via upload

---
+++ P4T1.py
@@ -0,0 +1,31 @@
+#CTI
+
+import turtle
+
+turtle.shape("turtle")
+
+turtle.pensize(3)            # increase pensize (takes integer)
+turtle.pencolor("purple")     # set pencolor (takes string)
+
+for i in range(4):
+    turtle.pencolor("purple") 
+
+    turtle.forward(50)
+    turtle.left(90)
+
+
+
+
+turtle.penup()
+turtle.forward(100)     
+turtle.pendown()
+
+
+#CTI pentagon
+
+for x in range(5):
+    turtle.forward(100)
+    turtle.left(72)
+    turtle.pencolor ("blue")
+    
+
