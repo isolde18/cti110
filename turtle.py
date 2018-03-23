9c2f7a81c3abd3476382cf62536f9bbd0016740b
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-03-23 12:57:16 -0400

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,22 @@
+#CTI
+#2.23.18
+#turtle graphics
+
+import turtle
+
+
+
+for i in range(4):
+    turtle.pencolor("purple") 
+
+    turtle.forward(50)
+    turtle.left(90)
+
+    
+#CTI pentagon
+
+for x in range(5):
+    turtle.forward(100)
+    turtle.left(72)
+    turtle.pencolor ("blue")
+    
