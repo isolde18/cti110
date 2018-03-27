cf1eab1703129334d095d8ac474fcdef70030e07
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-03-27 10:02:10 -0400

    Add files via upload

---
+++ function.py
@@ -0,0 +1,42 @@
+
+#27.02.18
+#CTI/def
+
+
+def main():
+
+    print ("Below is a description of how to boil water")
+    
+    def step_1():
+        print("step 1:")
+        print("Get a pot")
+
+    step_1()
+
+    def step_2():
+        print("step 2:")
+        print ("Fill pot with water")
+
+    step_2()
+
+    def step_3():
+        print("step 3:")
+
+        print("Put pot on stove")
+
+    step_3()
+
+    def step_4():
+
+        print("step 4:")
+        print("Turn burner on")
+
+    step_4()
+
+    def step_5():
+        print("step 5:")
+        print("Wait!!!!!")
+
+    step_5()
+
+main()
